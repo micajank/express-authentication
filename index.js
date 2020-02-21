b25922440d7bb082543743a191058a9785876874
micajank <micajank@umich.edu>
2020-02-21 14:02:52 -0800

    Everything working, plus helmet and session store

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const session = require("express-session");
 const passport = require("./config/ppConfig");
 const flash = require("connect-flash");
 const isLoggedIn = require("./middleware/isLoggedIn");
+const helmet = require("helmet");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const db = require("./models");
 
 const app = express();
 
@@ -15,12 +18,21 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(ejsLayouts);
+app.use(helmet());
+
+const sessionStore = new SequelizeStore({
+  db: db.sequelize,
+  expiration: 1000 * 60 * 30
+})
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: sessionStore
 }));
+
+sessionStore.sync();
 
 app.use(passport.initialize());
 app.use(passport.session());
